3a022066ce9e11e9a3905084500816980d1005c7
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-04-06 23:45:40 +0300

    Pin fingerprint hal from 9.0.10

--- device.mk
+++ device.mk
@@ -300,8 +300,9 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal \
-    sensors.nubia \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.nubia
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
