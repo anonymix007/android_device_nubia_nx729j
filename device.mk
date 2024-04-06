4e2eda165a11440c4015ba4c6f239ee24b9910f1
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-04-06 16:50:22 +0300

    fixup! Drop prebuilt android.hardware.sensors@2.1-service.multihal

--- device.mk
+++ device.mk
@@ -299,7 +299,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service-multihal \
+    android.hardware.sensors@2.1-service.multihal \
     sensors.nubia \
     libsensorndkbridge
 
