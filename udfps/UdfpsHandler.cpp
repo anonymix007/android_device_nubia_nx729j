07ab23ade2fc74a4e10e250754885d8b4f093cc7
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-04-09 15:45:24 +0300

    Call sendCustomizedCommand in onFingerDown and onFingerUp
    Also remove udfps uevent-based handler as the same can be done through HIDL fingerprint interface

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -4,89 +4,57 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "UdfpsHander.nubia"
+#define LOG_TAG "UdfpsHandler.nubia"
 
 #include "UdfpsHandler.h"
 
 #include <android-base/logging.h>
+#include <log/log.h>
 #include <fcntl.h>
 #include <poll.h>
 #include <thread>
 #include <unistd.h>
-#include <cutils/uevent.h>
+#include <cstring>
 
-#include <vendor/goodix/hardware/biometrics/fingerprint/2.1/IGoodixFingerprintDaemon.h>
+typedef struct fingerprint_device_gf95xx{
+    hw_device_t common;
+    fingerprint_notify_t notify;
+    int (*set_notify)(struct fingerprint_device *dev, fingerprint_notify_t notify);
+    uint64_t (*pre_enroll)(struct fingerprint_device *dev);
+    int (*enroll)(struct fingerprint_device *dev, const hw_auth_token_t *hat, uint32_t gid, uint32_t timeout_sec);
+    int (*post_enroll)(struct fingerprint_device *dev);
+    uint64_t (*get_authenticator_id)(struct fingerprint_device *dev);
+    int (*cancel)(struct fingerprint_device *dev);
+    int (*enumerate)(struct fingerprint_device *dev);
+    int (*remove)(struct fingerprint_device *dev, uint32_t gid, uint32_t fid);
+    int (*set_active_group)(struct fingerprint_device *dev, uint32_t gid, const char *store_path);
+    int (*authenticate)(struct fingerprint_device *dev, uint64_t operation_id, uint32_t gid);
+    int (*sendCustomizedCommand)(fingerprint_device_gf95xx *device, uint32_t cmd, uint32_t extras, const char *msg, ssize_t len);
+    void *reserved[4];
+} fingerprint_device_gf95xx_t;
 
-#include <sensors/UEvent.h>
-
-
-#define NOTIFY_FINGER_DOWN 1536
-#define NOTIFY_FINGER_UP 1537
-
-
-#define TP_EVENT_PATH "MODALIAS=platform:zte_touch"
-#define SINGLE_TAP_GESTURE "single_tap"
-#define DOUBLE_TAP_GESTURE "double_tap"
-#define AOD_AREAMEET_DOWN "aod_areameet_down"
-#define AREAMEET_DOWN "areameet_down"
-#define AREAMEET_UP "areameet_up"
-#define UEVENT_BUFFER_SIZE 8192
-
-using ::android::sp;
-using ::android::hardware::hidl_vec;
-using ::vendor::goodix::hardware::biometrics::fingerprint::V2_1::IGoodixFingerprintDaemon;
+#define CUSTOMIZED_COMMAND "FingerService"
+#define CUSTOMIZED_COMMAND_LEN strlen("FingerService")
 
 class NubiaUdfpsHander : public UdfpsHandler {
   public:
     void init(fingerprint_device_t *device) {
-        mDevice = device;
-
-        mGoodixFingerprintDaemon = IGoodixFingerprintDaemon::getService();
-
-        std::thread([this]() {
-            int fd = uevent_open_socket(256 * 1024, false);
-
-            fcntl(fd, F_SETFL, O_NONBLOCK);
-
-            if (fd < 0) {
-                LOG(ERROR) << "failed to open fd, err: " << fd;
-                return;
-            }
-
-            struct pollfd ufd = {
-                    .fd = fd,
-                    .events = POLLERR | POLLPRI,
-                    .revents = 0,
-            };
-
-            while (true) {
-                int rc = poll(&ufd, 1, -1);
-                if (rc < 0) {
-                    LOG(ERROR) << "failed to poll fd, err: " << rc;
-                    continue;
-                }
-
-                if (ufd.revents & POLLIN) {
-                    int command = -1;
-                    while(ReadUevent(fd, &command)) {
-                        if (command > 0) {
-                            mGoodixFingerprintDaemon->sendCommand(command, {}, [](int, const hidl_vec<signed char>&) {});
-                        }
-                    }
-                }
-            }
-        }).detach();
+        mDevice = reinterpret_cast<fingerprint_device_gf95xx_t *>(device);
     }
 
-    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
-        // nothing
+    void onFingerDown(uint32_t x, uint32_t y, float /*minor*/, float /*major*/) {
+        if (mDevice->sendCustomizedCommand) {
+            mDevice->sendCustomizedCommand(mDevice, 10, 1, CUSTOMIZED_COMMAND, CUSTOMIZED_COMMAND_LEN);
+        }
     }
 
     void onFingerUp() {
-        // nothing
+        if (mDevice->sendCustomizedCommand) {
+            mDevice->sendCustomizedCommand(mDevice, 10, 0, CUSTOMIZED_COMMAND, CUSTOMIZED_COMMAND_LEN);
+        }
     }
 
-    void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
+    void onAcquired(int32_t result, int32_t vendorCode) {
         // nothing
     }
 
@@ -95,51 +63,7 @@ class NubiaUdfpsHander : public UdfpsHandler {
     }
 
   private:
-    fingerprint_device_t *mDevice;
-    sp<IGoodixFingerprintDaemon> mGoodixFingerprintDaemon;
-    char buf[UEVENT_BUFFER_SIZE + 2];
-
-    int ParseUevent() {
-        android::hardware::sensors::UEvent event(buf);
-
-        if (!event.contains(TP_EVENT_PATH)) {
-            return -1;
-        }
-        LOG(DEBUG) << "Processing zte_touch uevent!";
-
-        if (event.get(AREAMEET_DOWN, "false") == "true" || event.get(AOD_AREAMEET_DOWN, "false") == "true") {
-			return NOTIFY_FINGER_DOWN;
-		}
-
-		if (event.get(AREAMEET_UP, "false") == "true") {
-			return NOTIFY_FINGER_UP;
-		}
-
-        return -1;
-    }
-
-    bool ReadUevent(int fd, int *command) {
-        int n = uevent_kernel_multicast_recv(fd, buf, UEVENT_BUFFER_SIZE);
-        if (n <= 0) {
-            if (errno != EAGAIN && errno != EWOULDBLOCK) {
-                LOG(ERROR) << "Error reading from uevent fd";
-            }
-            return false;
-        }
-        if (n >= UEVENT_BUFFER_SIZE) {
-            LOG(ERROR) << "Uevent overflowed buffer, discarding";
-            // Return true here even if we discard as we may have more uevents pending and we
-            // want to keep processing them.
-            return true;
-        }
-
-        buf[n] = '\0';
-        buf[n + 1] = '\0';
-
-        *command = ParseUevent();
-
-        return true;
-    }
+    fingerprint_device_gf95xx_t *mDevice;
 };
 
 static UdfpsHandler* create() {
