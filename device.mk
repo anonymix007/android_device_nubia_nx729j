1e1ba13808612564918b6c4b23bd15c77c04cd88
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-05-07 23:11:16 +0300

    Switch to default aidl multihal

--- device.mk
+++ device.mk
@@ -297,7 +297,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors-service.multihal \
     libsensorndkbridge \
     sensors.nubia
 
