c65debbd3d0d55d3d06a0a81a0a1afb726fbb7b3
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-09-26 10:00:03 +0200

    NX729J: Allow gettid syscall for wfdhdcphalservice
    09-25 22:37:52.968 16288 16288 E wfdhdcphal: wfdhdcphalservice starting...
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: failed to get path of fd 5: No such file or directory
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: connect
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: fcntl
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: sendto
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: socket
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: writev
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(33): nonexistent syscall 'fstatat'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(52): nonexistent syscall 'open'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(53): nonexistent syscall 'ugetrlimit'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(64): nonexistent syscall 'readlink'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: logging seccomp filter failures
    09-25 22:37:52.971 16288 16288 E wfdhdcphalservice: libminijail[16288]: blocked syscall: gettid
    09-25 22:37:52.987     1     1 I init    : Service 'wfdhdcphalservice' (pid 16288) received signal 31
    09-25 22:37:52.987     1     1 I init    : Sending signal 9 to service 'wfdhdcphalservice' (pid 16288) process group...
    
    Change-Id: I9b33dd63b408444f26382aaf2b7647ae00500c08

--- extract-files.sh
+++ extract-files.sh
@@ -89,7 +89,8 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libjnigraphics.so" "libcv_shim.so" "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+            [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
         vendor/lib64/vendor.libdpmframework.so)
